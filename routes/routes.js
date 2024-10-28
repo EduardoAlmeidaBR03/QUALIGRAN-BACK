c309bd201b0ca0f393b28e8e25ff4a96d061789c
EduardoAlmeidaBR03 <eduardo.almeida09@hotmail.com>
2024-10-28 10:53:41 -0300

    add delete

--- routes/routes.js
+++ routes/routes.js
@@ -1,37 +1,39 @@
 ////////////////MÓDULOS /////////////////////
 const express = require("express");
 const router = express.Router();
+
 //////CONTROLLERS
 const usuarioController = require("../controller/usuarioController");
 const chapaController = require("../controller/chapaController");
+
 ////////////////Requisições HTTP Principal /////////////////////
-router.get("/",(req, res) =>{
- return res.json({message: "Sistema Qualigran"});
-})
+router.get("/", (req, res) => {
+    return res.json({ message: "Sistema Qualigran" });
+});
+
 ////////////////Requisições HTTP Usuario /////////////////////
 
-//POST - CADASTRAR
+//POST - CADASTRAR USUÁRIO
 router.post("/add_usuario", usuarioController.UsuarioCreate);
 
-//GET - LISTAR
-router.get("/usuarios/:id?",usuarioController.verificaJWT,usuarioController.UsuarioListar);
+//GET - LISTAR USUÁRIOS (com ID opcional)
+router.get("/usuarios/:id?", usuarioController.UsuarioListar);
 
-//PUT - UPDATE
+//PUT - ATUALIZAR USUÁRIO
 router.put("/usuarios/:id", usuarioController.UsuarioUpdate);
 
-// DELETE
+// DELETE - EXCLUIR USUÁRIO
 router.delete("/usuarios/:id", usuarioController.UsuarioDelete);
 
+// POST - LOGIN (VERIFICAR LOGIN DO USUÁRIO)
 router.post("/login", usuarioController.UsuarioVerificaLogin);
 
+/////////////////Requisições Produto /////////////////////
 
-
-/////////////////Requisições Produto
-
-//POST - CADASTRAR
+//POST - CADASTRAR CHAPA
 router.post("/add_chapa", chapaController.ChapaCreate);
 
-//GET - LISTAR
+//GET - LISTAR CHAPAS (com ID opcional)
 router.get("/chapas/:id?", chapaController.ChapaListar);
 
 module.exports = router;
